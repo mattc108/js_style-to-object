6578ae3a7504a4ae97b748f449975cebeca427a6
mattc108 <matthewcamacho108@gmail.com>
2025-06-30 21:07:01 -0500

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -7,6 +8,31 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const editedString = sourceString.replace('\n', '');
+  const properties = editedString.split(';');
+
+  const result = {};
+
+  for (const p of properties) {
+    let prop = p;
+
+    prop = prop.replace('\n', '');
+    prop = prop.replace('\t', '');
+    prop = prop.trim();
+
+    if (prop.length === 0) {
+      continue;
+    }
+
+    const property = prop.split(':');
+
+    property[0] = property[0].trim();
+    property[1] = property[1].trim();
+
+    result[property[0]] = property[1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
